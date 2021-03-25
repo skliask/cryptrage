14a440dbb635c1bffa45890bfeb4508e4b4bbd9f
Kostas Sklias <k@sklias.me>
2021-03-26 01:50:26 +0200

    Adds pagination

--- app/controllers/margins_controller.rb
+++ app/controllers/margins_controller.rb
@@ -1,5 +1,7 @@
 class MarginsController < ApplicationController
   def index
-    @margins = Margin.includes(base: [:base,:quote], quote: [:base,:quote]).where("margin > 0").order(margin: :desc).limit(100)
+    @margins = Margin.includes(base: [:base,:quote], quote: [:base,:quote])
+      .where("margin > 0").order(margin: :desc).limit(100)
+      .page(params[:page]).per(20)
   end
 end
--- app/controllers/triangles_controller.rb
+++ app/controllers/triangles_controller.rb
@@ -1,6 +1,6 @@
 class TrianglesController < ApplicationController
   def index
-    @triangles = Triangle.where("rate < ?", 26).includes([:pair_a, :pair_b, :pair_c]).limit(100)
-      .order(rate: :desc)
+    @triangles = Triangle.where("rate < ?", 26).includes([:pair_a, :pair_b, :pair_c])
+      .order(rate: :desc).page(params[:page]).per(20)
   end
 end
